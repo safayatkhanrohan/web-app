40e4fa4543565fdced435356c5e02b1eba100152
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2023-10-06 19:12:39 +0600

    first-commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const email = req.body.email;
+  const userData = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+
+  var jsonData = JSON.stringify(userData);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/ceed8b9aa1";
+
+  const options = {
+    method: "POST",
+    auth: "rohan28:49ef5685784f3db42544f42c190eaae5-us21",
+  };
+
+  // seding data to mailchimp start
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server has started on port 3000.");
+});
+
